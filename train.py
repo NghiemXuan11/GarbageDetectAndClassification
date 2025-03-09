ed065b54a736b9cbd425dfd6c31fdf0a0ece5f20
Nghiem Nguyen <11112001nghiemit@gmail.com>
2025-03-09 15:57:33 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import onnx
-model = YOLO('yolov8n.yaml')
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov9n.yaml')
+model = YOLO('yolov9n.pt')
 path = '/content/datasets/waste-detection-9/data.yaml'
 results = model.train(data=path, epochs=50)
 results = model.val()
